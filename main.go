9543a13d6fdaed7ddba9d955f82f89055808f26b
Nikita Melnikov <nikmel2803@gmail.com>
2021-07-03 23:35:01 +0300

    send telegram notifications

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"sync"
 	"time"
 
@@ -15,14 +19,19 @@ import (
 	"go.mongodb.org/mongo-driver/x/mongo/driver/connstring"
 )
 
+// Watch describes structure for collection watch task
 type Watch struct {
 	Name       string `yaml:"name"`
 	On         string `yaml:"on"`
 	Collection string `yaml:"collection"`
+	NotifyHook string `yaml:"notify_hook"`
+	Template   string `yaml:"template"`
 }
 
+// Config describes application configuration structure
 type Config struct {
-	MongodbUri string  `yaml:"mongodbUri"`
+	MongodbUri string  `yaml:"mongodb_uri"`
+	NotifyHook string  `yaml:"notify_hook"`
 	Watches    []Watch `yaml:"watches"`
 }
 
@@ -69,23 +78,38 @@ func main() {
 	var waitGroup sync.WaitGroup
 
 	for _, value := range cfg.Watches {
-		collection := database.Collection(value.Collection)
-
-		stream, err := collection.Watch(ctx, mongo.Pipeline{})
-		if err != nil {
-			panic(err)
-		}
-
-		waitGroup.Add(1)
-		routineCtx, _ := context.WithCancel(context.Background())
-
-		go iterateChangeStream(routineCtx, waitGroup, stream)
+		setupCollectionWatch(value, database, ctx, &waitGroup)
 	}
 
 	waitGroup.Wait()
 }
 
-func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, stream *mongo.ChangeStream) {
+// Pretty-prints data as JSON document
+func printJson(data interface{}) {
+	text, err := json.MarshalIndent(data, "", "\t")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(text))
+}
+
+func setupCollectionWatch(watch Watch, database *mongo.Database, ctx context.Context, waitGroup *sync.WaitGroup) {
+	collection := database.Collection(watch.Collection)
+
+	stream, err := collection.Watch(ctx, mongo.Pipeline{}, options.ChangeStream().SetFullDocument(options.UpdateLookup))
+	if err != nil {
+		panic(err)
+	}
+
+	waitGroup.Add(1)
+	routineCtx, _ := context.WithCancel(context.Background())
+
+	go iterateChangeStream(routineCtx, waitGroup, stream, watch)
+}
+
+func iterateChangeStream(routineCtx context.Context, waitGroup *sync.WaitGroup, stream *mongo.ChangeStream, watch Watch) {
 	defer stream.Close(routineCtx)
 	defer waitGroup.Done()
 	for stream.Next(routineCtx) {
@@ -93,6 +117,50 @@ func iterateChangeStream(routineCtx context.Context, waitGroup sync.WaitGroup, s
 		if err := stream.Decode(&data); err != nil {
 			panic(err)
 		}
-		fmt.Printf("%v\n", data)
+
+		notify(watch.Template, watch.NotifyHook)
+		printJson(data)
 	}
+}
+
+func notify(text string, hook string) error {
+	data := url.Values{}
+	data.Set("message", text)
+
+	_, err := MakeHTTPRequest("POST", hook, []byte(data.Encode()), map[string]string{
+		"Content-Type": "application/x-www-form-urlencoded",
+	})
+	if err != nil {
+		return fmt.Errorf("Webhook error: %v", err)
+	}
+
+	return nil
+}
+
+// MakeHTTPRequest - make HTTP request with specified method, body, URL and headers
+func MakeHTTPRequest(method string, url string, body []byte, headers map[string]string) ([]byte, error) {
+	client := &http.Client{}
+	r := bytes.NewReader(body)
+	req, err := http.NewRequest(method, url, r)
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
